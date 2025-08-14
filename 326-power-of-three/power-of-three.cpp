82075a13d560585cfe31a8e427a27a40ebe254ed
niteshsah45 <shahnitesh878@gmail.com>
2025-08-14 21:35:14 +0530

    Time: 2 ms (66.51%) | Memory: 9 MB (46.40%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -5,11 +5,6 @@ public:
          while(n%3==0){
             n=n/3;
          }
-        if(n==1){
-            return true;
-        }
-        else{
-                return false;
-        } 
+        return n==1;
     }
 };
