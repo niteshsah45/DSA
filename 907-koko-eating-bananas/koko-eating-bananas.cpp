5d9771e4d29e102ceaa3e8f9bd324e6f8541c9de
niteshsah45 <shahnitesh878@gmail.com>
2025-08-26 22:21:32 +0530

    Time: 5 ms (81.82%) | Memory: 22.8 MB (96.22%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    // finding the each totaltime with each number from 1 to max in the array
-    long long findingtotaltime(vector<int> &piles,int mid){
-        long long total=0;
-        for(int i=0;i<piles.size();i++){
-            total+=(piles[i]+mid-1)/mid;// use ceil function to get roundup in ceilling example 1.5 to 2
-        }
-        return total;
+long long kokospeed(vector<int>& piles,int mid, int h){
+    int n=piles.size();
+    long long hours=0;
+    for(int i=0;i<n;i++){
+        hours+=(piles[i]+mid-1LL)/mid;
     }
+    return hours;
+}
     int minEatingSpeed(vector<int>& piles, int h) {
-        // find the totaltimerequired using binary search which is less than give hours
-        long long low=1,high=*max_element(piles.begin(),piles.end());
-        long long ans=high;
+        int low=1;
+        int high=*max_element(piles.begin(),piles.end());
         while(low<=high){
-            long long mid=low+(high-low)/2;
-            long long totaltime=findingtotaltime(piles,mid);
-            if(totaltime<=h){
-                ans=mid;
+            int mid=low+(high-low)/2;
+            long long result= kokospeed(piles,mid,h);
+            if(result<=h){
                 high=mid-1;
             }
             else{
                 low=mid+1;
             }
         }
-        return (int)ans;
+        return low;
     }
 };
