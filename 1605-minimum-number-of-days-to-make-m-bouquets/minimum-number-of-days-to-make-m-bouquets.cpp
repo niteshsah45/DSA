d7308ba0bfdec27d0909074e2c880dfd47cd6b6c
niteshsah45 <shahnitesh878@gmail.com>
2025-08-26 21:56:22 +0530

    Time: 20 ms (72.40%) | Memory: 70.4 MB (97.58%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
-bool possiblebloomday(vector<int> &bloomDay,int mid,int m,int k){
-    int n=bloomDay.size();
-    int noof_bouquets=0;
-    int count=0;
-    for(int i=0;i<n;i++){
-        if(bloomDay[i]<=mid){
-            count++;
+bool bloomcheck(vector<int>& bloomDay, int mid,int m,int k){
+        int n=bloomDay.size();
+        int countb=0;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(bloomDay[i]<=mid){
+                count++;
+                if(count>=k){
+                    countb++;
+                    count=0;
+                }
+            }
+            else{
+                count=0;
+            }
+        }
+        if(countb>=m){
+            return true;
         }
         else{
-            noof_bouquets+=count/k;
-            count=0;
+            return false;
         }
-    }
-    noof_bouquets+=count/k;
-    if(noof_bouquets>=m) return true;
-    else return false;
 }
     int minDays(vector<int>& bloomDay, int m, int k) {
         int n=bloomDay.size();
         if((long long)m*k>n) return -1;
         int low=*min_element(bloomDay.begin(),bloomDay.end());
         int high=*max_element(bloomDay.begin(),bloomDay.end());
-        int store=-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            bool result = possiblebloomday(bloomDay,mid,m,k);
+            bool result= bloomcheck(bloomDay,mid,m,k);
             if(result){
-                store=mid;
                 high=mid-1;
             }
             else{
                 low=mid+1;
             }
-        } 
-        return store;  
-        
+        }
+        return low;     
     }
 };
