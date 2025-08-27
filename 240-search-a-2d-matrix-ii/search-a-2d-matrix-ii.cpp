9011b7eeb64afb6a393664b8be92442e35bc9c55
niteshsah45 <shahnitesh878@gmail.com>
2025-08-27 20:07:20 +0530

    Time: 59 ms (30.74%) | Memory: 18.8 MB (36.48%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
-bool binarysearch(vector<int>& matrix,int target){
-    int low=0;
-    int high=matrix.size()-1;
-    while(low<=high){
-       int mid=low+(high-low)/2;
-       if(matrix[mid]==target){
-        return true;
-       } 
-       else if(matrix[mid]>target){
-        high=mid-1;
-       }
-       else{
-        low=mid+1;
-       }
-    }
-    return false;
-}
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int rows=matrix.size();
-        int cols=matrix[0].size();
-        for(int i=0;i<rows;i++){
-              bool result= binarysearch(matrix[i],target);
-              if(result){
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int row=0,col=m-1;
+        while(row<n && col>=0){
+            if(matrix[row][col]==target){
                 return true;
-              } 
+            }
+            else if(matrix[row][col]<target){// 15 for target 5 is greater so 5 will never lie in 15 to 30 it maybe lies in 15 to 1 so eleminate the col
+                row++;
+            }
+            else{
+                col--;
+            }
         }
-        return false; 
+        return false;
+        
     }
 };
