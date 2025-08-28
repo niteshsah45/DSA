a33cf4a0e6378d4e6876a4d13bfba88b7f7f8af9
niteshsah45 <shahnitesh878@gmail.com>
2025-08-28 23:16:12 +0530

    Time: 8 ms (25.54%) | Memory: 26.6 MB (48.03%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+bool validPalin(string s,int left,int right){
+    while(left<right){
+        if(s[left]!=s[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
+}
+    bool validPalindrome(string s) {
+        int left=0,right=s.size()-1;
+        while(left<right){
+            if(s[left]!=s[right]){
+                return validPalin(s,left+1,right) || validPalin(s,left,right-1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
