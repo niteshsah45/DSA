8e42e9eba6dfb550bfd055e57285118337648a25
niteshsah45 <shahnitesh878@gmail.com>
2025-09-11 19:35:00 +0530

    Time: 19 ms (35.71%) | Memory: 18 MB (16.50%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,45 +1,36 @@
 class Solution {
 public:
     string sortVowels(string s) {
+        string storeVowelC="";
+        string storeVowelL="";
+        string temp=s;
         int n=s.size();
-        vector<int> freq(52,0);
         for(int i=0;i<n;i++){
-            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
-                freq[26+(s[i]-'a')]++;
+            if(s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
+                {
+                    	storeVowelC+=s[i];
+                        s[i]='#';
+             }
+            else if(s[i]=='a'|| s[i]=='e'||s[i]=='i'|| s[i]=='o'||s[i]=='u'){
+                storeVowelL+=s[i];
                 s[i]='#';
-            }
-            else if(s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
-                freq[s[i]-'A']++;
-                s[i]='#';
-            }
+             }
         }
-        string ans="";
-        for(int i=0;i<26;i++){
-            char c='A'+i;
-            while(freq[i]){
-                ans+=c;
-                freq[i]--;
-            }
-        }
-        for(int i=0;i<26;i++){
-            char c='a'+i;
-            while(freq[26+i]){
-                ans+=c;
-                freq[26+i]--;
-            }
-        }
+        if(storeVowelC.empty() && storeVowelL.empty()) return temp;
+        sort(storeVowelC.begin(),storeVowelC.end());
+        sort(storeVowelL.begin(),storeVowelL.end());
         string result="";
+        result+=storeVowelC;
+        result+=storeVowelL;
+        int n1=result.size();
         int j=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n|| j<n1;i++){
             if(s[i]=='#'){
-                result+=ans[j];
+                s[i]=result[j];
                 j++;
             }
-            else{
-                result+=s[i];
-            }
         }
-        if(result.size()==0) return s;
-        else return result;
+        return s;
+        
     }
 };
