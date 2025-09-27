f5630e978fcea6d7b7d269fef4783c3494e4a426
niteshsah45 <shahnitesh878@gmail.com>
2025-09-27 22:14:03 +0530

    Finding pair sum with optimized with o(n)

--- Linked_List/pairs-of-sum.cpp
+++ Linked_List/pairs-of-sum.cpp
@@ -3,10 +3,12 @@ using namespace std;
 struct Node{
     int data;
     Node* next;
+    Node* back;
     public:
-    Node(int data1,Node* next1=nullptr){
+    Node(int data1,Node* next1=nullptr,Node* back1=nullptr){
         data=data1;
         next=next1;
+        back= back1;
     }
 };
 Node* convert2LL(vector<int>& arr){
@@ -14,24 +16,33 @@ Node* convert2LL(vector<int>& arr){
     Node* prev=head;
     for(int i=1;i<arr.size();i++){
         Node* newNode = new Node(arr[i]);
+        newNode->back= prev;
         prev->next= newNode;
         prev= newNode;
     }
     return head;
 };
 vector<vector<int>> pairSum(Node* head,int key){
-    Node* temp1=head;
-    vector<vector<int>> result;
-    while(temp1!=NULL){
-        Node* temp2 = temp1->next;
-        while(temp2!=NULL && temp1->data + temp2->data<=key){
-            if(temp2->data+temp1->data==key){
-                result.push_back({temp1->data,temp2->data});
-            }
-            temp2=temp2->next;
-        }
-        temp1=temp1->next;
+    Node* left=head;
+    Node* right=head;
+    while(right->next!=NULL){
+        right=right->next;
     }
+    vector<vector<int>> result;
+    while(left->data<right->data){
+        if(left->data+right->data==key){
+            result.push_back({left->data,right->data});
+            left=left->next;
+            right=right->back;
+        }
+        else if(left->data+right->data>key){
+            right=right->back;
+        }
+        else{
+            left=left->next;
+        }
+    }
+
     return result;
 }
 void print(Node* head){
