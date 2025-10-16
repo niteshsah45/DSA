65452719ac95d56bd36bf069647038424dcf94be
niteshsah45 <shahnitesh878@gmail.com>
2025-10-16 23:11:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (37.73%) - LeetSync

---
+++ Recursion/29-divide-two-integers/divide-two-integers.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (divisor==0) return 0;
+        if(dividend==INT_MIN && divisor ==-1) return INT_MAX;
+        return dividend/divisor;
+    }
+};
