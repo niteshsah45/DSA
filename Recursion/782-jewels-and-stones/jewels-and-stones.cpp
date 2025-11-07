8e4b5eb3f8dd663be4768b01d31e9293b48799fb
niteshsah45 <shahnitesh878@gmail.com>
2025-11-07 22:47:07 +0530

    Time: 2 ms (18.76%) | Memory: 8.9 MB (7.76%) - LeetSync

---
+++ Recursion/782-jewels-and-stones/jewels-and-stones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_map<char,int> freqJ;
+        unordered_map<char,int> freqS;
+        int count=0;
+
+        for(auto c:jewels) freqJ[c]++;
+        for(auto c:stones) freqS[c]++;
+
+        for(auto c:jewels){
+            if(freqS.count(c)) count+=freqS[c];
+        }
+        return count;
+    }
+};
