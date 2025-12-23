a48fadf8717b71433f990aaa40c95ebf4043b95c
niteshsah45 <shahnitesh878@gmail.com>
2025-12-23 09:46:56 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (14.71%) - LeetSync

--- Binary_Tree/222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ Binary_Tree/222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -12,31 +12,9 @@
 class Solution {
 public:
     int countNodes(TreeNode* root) {
+
+        if(!root) return 0;
+        return 1+ countNodes(root->left)+countNodes(root->right);
         
-        if(root==NULL) return 0;
-
-        int lh = findLeftHeight(root);
-        int rh = findRightHeight(root);
-
-        if(lh==rh) return (1<<lh)-1;
-
-        return 1 + countNodes(root->left) + countNodes(root->right);
-    }
-
-    int findLeftHeight(TreeNode* root){
-        int h =0;
-        while(root){
-            h++;
-            root=root->left;
-        }
-        return h;
-    }
-    int findRightHeight(TreeNode* root){
-        int h =0;
-        while(root){
-            h++;
-            root=root->right;
-        }
-        return h;
     }
 };
