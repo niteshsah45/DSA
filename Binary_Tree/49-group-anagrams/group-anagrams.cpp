91ed04ed27701aee2ca1c8573e5a417c65a96bed
niteshsah45 <shahnitesh878@gmail.com>
2025-12-30 22:08:28 +0530

    Time: 71 ms (10.35%) | Memory: 34.4 MB (8.53%) - LeetSync

--- Binary_Tree/49-group-anagrams/group-anagrams.cpp
+++ Binary_Tree/49-group-anagrams/group-anagrams.cpp
@@ -1,41 +1,26 @@
 class Solution {
 public:
-bool checkanna(string& s1,string& s2){
-    if(s1.size()!=s2.size()) return false;
-    vector<int> freq(26,0);
-
-    for(char ch:s1){
-        freq[ch-'a']++;
-    }
-    for(char ch:s2){
-        freq[ch-'a']--;
-    }
-    for(int i=0;i<freq.size();i++){
-        if(freq[i]!=0) return false;
-    }
-    return true;
-}
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> ans;
-        int n = strs.size();
-        vector<bool> used(n,false);
-        for(int i=0;i<n;i++){
-            if(used[i]) continue;
-            vector<string> level;
-            string s1 = strs[i];
-            used[i]=true;
-            level.push_back(s1);
-            for(int j=i+1;j<n;j++){
-                if(used[j]) continue;
-                string s2 = strs[j];
-                bool result = checkanna(s1,s2);
-                if(result && !used[j]){
-                    level.push_back(s2);
-                    used[j]=true;
-                }
+        unordered_map<string,vector<string>> mp;
+
+        for(string &s:strs){
+
+            vector<int> freq(26,0);
+            for(char ch:s){
+                freq[ch-'a']++;
             }
-            ans.push_back(level);
+            string keyval;
+            for(int i=0;i<26;i++){
+                keyval+=to_string(freq[i])+"#";
+            }
+
+            mp[keyval].push_back(s);
+        }
+        vector<vector<string>> ans;
+        for(auto &p:mp){
+            ans.push_back(p.second);
         }
         return ans;
+        
     }
 };
