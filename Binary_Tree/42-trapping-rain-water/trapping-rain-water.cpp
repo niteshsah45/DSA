28e179fe74e4ad033a23b6280e67b5e677b3f511
niteshsah45 <shahnitesh878@gmail.com>
2025-12-31 21:21:04 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (58.00%) - LeetSync

---
+++ Binary_Tree/42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int left =0,right = n-1;
+        int leftMax =0,rightMax=0;
+        int totalWater =0;
+        while(left<right){
+
+            if(height[left]<=height[right]){
+
+                leftMax = max(leftMax,height[left]);
+                totalWater += leftMax - height[left];
+                left++;
+            }
+            else{
+
+                rightMax = max(rightMax,height[right]);
+                totalWater += rightMax - height[right];
+                right--;
+            }
+        }
+        return totalWater;
+    }
+};
