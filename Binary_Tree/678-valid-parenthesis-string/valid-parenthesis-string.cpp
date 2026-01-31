cdefafaf0e4f5c08061fb472ea704dd4de85e016
niteshsah45 <shahnitesh878@gmail.com>
2026-01-31 22:20:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (46.45%) - LeetSync

--- Binary_Tree/678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ Binary_Tree/678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     bool checkValidString(string s) {
 
-        int mini =0,maxi=0;
+        int maxi=0,mini=0;
+        int n = s.size();
 
-        for(int i=0;i<s.size();i++){
+        for(int i=0;i<n;i++){
 
             if(s[i]=='('){
-                mini = mini+1;
-                maxi=maxi+1;
+                maxi+=1;
+                mini+=1;
             }
             else if(s[i]==')'){
+                maxi-=1;
+                mini-=1;
+            }
+            else {
+                maxi+=1;
+                mini-=1;
+            }
 
-                mini=mini-1;
-                maxi=maxi-1;
-            }
-            else{
-                mini=mini-1;
-                maxi=maxi+1;
-            }
-             if(mini<0) mini=0;
-        if(maxi<0) return false;
+            if(mini<0) mini=0;
+            if(maxi<0) return false;
         }
-        
         return mini==0;
     }
 };
