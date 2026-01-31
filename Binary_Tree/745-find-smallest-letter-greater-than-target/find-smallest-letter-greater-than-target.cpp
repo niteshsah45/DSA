aef5378c0e3490740798b5a1b337c8784262020a
niteshsah45 <shahnitesh878@gmail.com>
2026-01-31 14:57:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (24.67%) - LeetSync

--- Binary_Tree/745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ Binary_Tree/745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -4,10 +4,19 @@ public:
 
         int n = letters.size();
         char ans = letters[0];
-        for(int i=0;i<n;i++){
 
-            if(letters[i]>target){
-                return letters[i];
+        int start=0,end=n-1;
+
+        while(start<=end){
+
+            int mid=start+(end-start)/2;
+
+            if(letters[mid]>target){
+                ans = letters[mid];
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
             }
         }
         return ans;
