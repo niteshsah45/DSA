324f85c603ccf5a4e03763222f8dc444b28de42e
niteshsah45 <shahnitesh878@gmail.com>
2026-02-03 15:10:03 +0530

    Time: 17 ms (34.00%) | Memory: 36.8 MB (16.06%) - LeetSync

--- Binary_Tree/1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ Binary_Tree/1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
 
-        vector<vector<int>> ans;
-
         sort(arr.begin(),arr.end());
 
-        int diff=INT_MAX;
+        vector<vector<int>> ans;
+
         int n = arr.size();
+        int diff=INT_MAX;
 
         for(int i=0;i<n-1;i++){
 
-            int check = abs(arr[i]-arr[i+1]);
-
-            diff=min(diff,check);
-
+            diff=min(diff,abs(arr[i]-arr[i+1]));
         }
+
         for(int i=0;i<n-1;i++){
 
-            int curr = abs(arr[i]-arr[i+1]);
+            int ansdiff = abs(arr[i]-arr[i+1]);
 
-            if(curr==diff){
+            if(ansdiff==diff){
                 ans.push_back({arr[i],arr[i+1]});
             }
         }
+
         return ans;
+        
     }
 };
