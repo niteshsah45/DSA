c79db619f53301fcef36988899cd64ce45a2b171
niteshsah45 <shahnitesh878@gmail.com>
2026-02-07 20:46:05 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (71.87%) - LeetSync

--- Binary_Tree/55-jump-game/jump-game.cpp
+++ Binary_Tree/55-jump-game/jump-game.cpp
@@ -2,17 +2,22 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
 
-        int maxIdx =0;
+
+        int ans =0;
+
         int n = nums.size();
-        //if(n==1) return true;
+
+       if(n==1) return true;
 
         for(int i=0;i<n;i++){
 
-            if(i> maxIdx) return false;
+            if(i!=n-1 && nums[i]==0 && nums[i]+i>=ans) return false;
 
-            maxIdx = max(maxIdx,nums[i]+i);
+            ans = max(ans,nums[i]+i);
         }
-        if(maxIdx>=n-1) return true;
+
+        if(ans>=n-1) return true;
         return false;
+        
     }
 };
