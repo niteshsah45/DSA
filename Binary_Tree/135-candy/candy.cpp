21dc3d176846a46418f38199ec081a7c47d26821
niteshsah45 <shahnitesh878@gmail.com>
2026-03-21 19:07:36 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (92.84%) - LeetSync

--- Binary_Tree/135-candy/candy.cpp
+++ Binary_Tree/135-candy/candy.cpp
@@ -2,41 +2,34 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
 
+        int sum =1;
         int n = ratings.size();
+        int i=1;
+        while(i<n){
 
-        vector<int> left(n);
-        vector<int> right(n);
-        left[0]=1;
-        right[n-1]=1;
-
-        for(int i=1;i<n;i++){
-
-            if(ratings[i]>ratings[i-1]){
-                left[i] = left[i-1]+1;
+            if(ratings[i]==ratings[i-1]){
+                sum+=1;
+                i++;
+                continue;
             }
-            else{
-                left[i]=1;
+            int peak=1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                peak++;
+                i++;
+                sum+=peak;
             }
-        }
-        for(int i=n-2;i>=0;i--){
-
-            if(ratings[i]>ratings[i+1]){
-                right[i]=right[i+1]+1;
+            int down =0;
+            while(i<n && ratings[i]<ratings[i-1]){
+                down++;
+                i++;
+                sum+=down;
             }
-            else{
-                right[i]=1;
+            down++;
+
+            if(peak<down){
+                sum+=(down-peak);
             }
-        }
-
-        int idx=0;
-        int ans=0;
-        while(n){
-
-                ans+=max(left[idx],right[idx]);
-                idx++;
-                n--;
-        }
-        return ans;
-        
+        }        
+        return sum;
     }
 };
