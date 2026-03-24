b1648c72a9e140d33e7f7ae2e20993b78570e43c
niteshsah45 <shahnitesh878@gmail.com>
2026-03-24 19:06:21 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (94.37%) - LeetSync

--- Binary_Tree/57-insert-interval/insert-interval.cpp
+++ Binary_Tree/57-insert-interval/insert-interval.cpp
@@ -2,27 +2,28 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
 
-        int n = intervals.size();
-        int i=0;
         vector<vector<int>> ans;
-        while(i<n && intervals[i][1]<newInterval[0]){
 
+        int i=0;
+        int n = intervals.size();
+
+        while(i<n && intervals[i][1]<newInterval[0]){
             ans.push_back(intervals[i]);
-            i=i+1;
+            i++;
         }
         while(i<n && intervals[i][0]<=newInterval[1]){
 
-            newInterval[0] = min(intervals[i][0],newInterval[0]);
-            newInterval[1]= max(intervals[i][1],newInterval[1]);
-            i=i+1;
-        } 
+            newInterval[0]= min(intervals[i][0],newInterval[0]);
+            newInterval[1]=max(intervals[i][1],newInterval[1]);
+            i++;
+        }
         ans.push_back(newInterval);
 
         while(i<n){
             ans.push_back(intervals[i]);
-            i=i+1;
+            i++;
         }
         return ans;
-
+        
     }
 };
