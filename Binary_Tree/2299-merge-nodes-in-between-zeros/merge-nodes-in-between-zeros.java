50644ea969792cb0f8eadb46e12f2dbf1fd6d166
niteshsah45 <shahnitesh878@gmail.com>
2026-03-31 15:04:57 +0530

    Time: 6 ms (71.65%) | Memory: 260.9 MB (31.60%) - LeetSync

---
+++ Binary_Tree/2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+
+         ListNode dummy = new ListNode(1);
+         ListNode ansNode = dummy;
+
+        ListNode temp = head.next;
+
+        int sum=0;
+        while(temp!=null){
+            
+            if(temp.val==0){
+                dummy.next = new ListNode(sum);
+                dummy = dummy.next;
+                sum=0;
+            }
+            else{
+                sum+=temp.val;
+            }
+
+            temp = temp.next;
+        }
+        return ansNode.next;
+    }
+}
