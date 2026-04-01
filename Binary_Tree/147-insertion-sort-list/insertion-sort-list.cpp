c17d2ce7630120d8c876cca02a27bd62a9eede5e
niteshsah45 <shahnitesh878@gmail.com>
2026-04-01 22:53:45 +0530

    Time: 57 ms (5.05%) | Memory: 14.6 MB (53.42%) - LeetSync

---
+++ Binary_Tree/147-insertion-sort-list/insertion-sort-list.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+
+        if(head==NULL || head->next==NULL) return head;
+
+        ListNode* temp =head;
+
+        int count=0;
+        while(temp!=NULL){
+            count++;
+            temp = temp->next;
+        }
+        temp = head;
+
+        for(int i=0;i<count-1;i++){
+
+            temp =head;
+
+            for(int j=0;j<count-i-1;j++){
+                if(temp->val>temp->next->val){
+                    swap(temp->val,temp->next->val);
+                }
+                temp = temp->next;
+            }
+        }
+        return head;
+
+        
+    }
+};
