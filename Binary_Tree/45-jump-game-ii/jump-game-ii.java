31e16e2dba5bd0756176f20a1f68a198fa917b06
niteshsah45 <shahnitesh878@gmail.com>
2026-04-12 00:25:43 +0530

    Time: 1 ms (99.59%) | Memory: 47.5 MB (26.21%) - LeetSync

--- Binary_Tree/45-jump-game-ii/jump-game-ii.java
+++ Binary_Tree/45-jump-game-ii/jump-game-ii.java
@@ -2,30 +2,23 @@ class Solution {
     public int jump(int[] nums) {
 
         int jumps=0,left=0,right=0;
+        int farthest=0;
 
         int n = nums.length;
 
-        int farthest=0;
-
         while(right<n-1){
 
-            for(int i=left;i<=right;i++){
+            for(int j=left;j<=right;j++){
 
-
-                int sum = nums[i]+i;
-
-                if(sum>farthest){
-                    farthest= sum;
-                }
+                farthest = Math.max(farthest,nums[j]+j);
+               
             }
+
             left = right+1;
             right = farthest;
             jumps++;
         }
 
         return jumps;
-
-        
-        
     }
 }
