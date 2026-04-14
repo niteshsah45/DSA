dfe47767fb9d5cb9f3e710ddf7ad839d08e442ec
niteshsah45 <shahnitesh878@gmail.com>
2026-04-14 20:35:10 +0530

    Time: 38 ms (96.36%) | Memory: 29 MB (91.86%) - LeetSync

--- Binary_Tree/15-3sum/3sum.cpp
+++ Binary_Tree/15-3sum/3sum.cpp
@@ -2,35 +2,43 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
 
-        sort(nums.begin(),nums.end());
 
-        vector<vector<int>> result;
+        sort(nums.begin(),nums.end());
         int n = nums.size();
+        vector<vector<int>> ans;
+
+        if(nums[0]>=1) return ans;
 
         for(int i=0;i<n-2;i++){
+
             if(i>0 && nums[i]==nums[i-1]) continue;
 
             int j=i+1;
             int k=n-1;
 
+            if(nums[i]>=1) return ans;
+
             while(j<k){
 
                 long long sum = (long long)nums[i]+nums[j]+nums[k];
+
                 if(sum==0){
-                    result.push_back({nums[i],nums[j],nums[k]});
+                     ans.push_back({nums[i],nums[j],nums[k]});
                     j++;
                     k--;
+
                     while(j<k && nums[j]==nums[j-1]) j++;
                     while(j<k && nums[k]==nums[k+1]) k--;
                 }
-                else if(sum>0){
-                    k--;
+                else if(sum<0){
+                    j++;
                 }
                 else{
-                    j++;
+                   k--;
                 }
             }
         }
-         return result;
+        return ans;
+        
     }
 };
