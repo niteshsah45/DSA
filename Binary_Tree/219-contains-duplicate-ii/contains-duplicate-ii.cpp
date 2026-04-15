c8a03d9759ebacd53945d51a548b0597f6b7f0e0
niteshsah45 <shahnitesh878@gmail.com>
2026-04-15 22:49:06 +0530

    Time: 75 ms (59.23%) | Memory: 98.8 MB (30.50%) - LeetSync

---
+++ Binary_Tree/219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+
+
+        unordered_map<int,int> mpp;
+
+        int n = nums.size();
+
+        for(int i=0;i<n;i++){
+
+            if(mpp.count(nums[i])){
+
+                int finale = abs(mpp[nums[i]]-i);
+
+                if(finale<=k) return true;
+            }
+
+            mpp[nums[i]]=i;
+        }
+
+        return false;
+        
+    }
+};
