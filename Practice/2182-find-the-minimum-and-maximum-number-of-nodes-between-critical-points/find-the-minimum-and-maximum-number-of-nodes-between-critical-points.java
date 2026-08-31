8c3338f8125c562e8fdcbdd0812759507561044a
niteshsah45 <shahnitesh878@gmail.com>
2026-08-31 23:01:02 +0530

    Time: 4 ms (99.87%) | Memory: 106.2 MB (55.32%) - LeetSync

--- Practice/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ Practice/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -11,12 +11,18 @@
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
 
-        List<Integer> st = new ArrayList<>();
+        //List<Integer> st = new ArrayList<>();
+
+        int prevcritical=0,firstcritical=0;
+        int mini=Integer.MAX_VALUE;
+        int maxi=Integer.MIN_VALUE;
+        boolean check=false;
 
         ListNode temp = head.next;
         int[] ans = {-1,-1};
 
         ListNode prev = head;
+
 
         int count=1;
 
@@ -24,36 +30,49 @@ class Solution {
 
             count++;
 
-                if(prev.val>temp.val && temp.next.val>temp.val){
-                    st.add(count);
-                }
-                else if(prev.val<temp.val && temp.next.val<temp.val){
+                if(prev.val>temp.val && temp.next.val>temp.val ||
+                    prev.val<temp.val && temp.next.val<temp.val){
 
-                    st.add(count);
-                }
+                    if(!check){
+                        firstcritical=count;
+                        prevcritical=count;
+                        check=true;
+                    }
+                    else{
 
-            prev=temp;
-            temp = temp.next;
+                        mini = Math.min(mini,count-prevcritical);
+
+                        prevcritical=count;
+                    }
+
+                }  
+
+                prev=temp;
+                temp = temp.next;
         }
-        Collections.sort(st);
-        int len = st.size();
+        // Collections.sort(st);
+        // int len = st.size();
 
-        if(len<2) return ans;
-        System.out.print(st.get(0));
+        // if(len<2) return ans;
+        // System.out.print(st.get(0));
 
-        int maxi = st.get(len-1)-st.get(0);
+        // int maxi = st.get(len-1)-st.get(0);
 
-        int mini =Integer.MAX_VALUE;
+        // int mini =Integer.MAX_VALUE;
 
-        for(int i=0;i<len-1;i++){
+        // for(int i=0;i<len-1;i++){
 
-            int result = st.get(i+1)-st.get(i);
+        //     int result = st.get(i+1)-st.get(i);
 
-            if(result<mini){
+        //     if(result<mini){
 
-                mini=result;
-            }
-        }
+        //         mini=result;
+        //     }
+        // }
+
+        if(!check || firstcritical==prevcritical) return ans;
+
+        maxi = prevcritical-firstcritical;
         ans[0]=mini;
         ans[1]=maxi;
         return ans;
